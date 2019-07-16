4bbeb96b24e05b3487286030e463afd41918c8b6
John Rappel <john_rappel@epam.com>
2019-07-16 11:27:01 -0500

    test with environment variable

--- Deploy-SolrAzureAppService.ps1
+++ Deploy-SolrAzureAppService.ps1
@@ -4,15 +4,16 @@ Param(
 
 $solrName = "solr-$solrVersion"
 
-#Write-Output 'Copy wwwroot folder'
-#xcopy wwwroot ..\wwwroot /Y
-
 Write-Output 'Setting Security to TLS 1.2'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Output 'Prevent the progress meter from trying to access the console'
 $global:progressPreference = 'SilentlyContinue'
 
-Write-Output "Downloading Solr $solrVersion"
+$homeDirectory = env:HOME
+
+$siteRoot = "$homeDirectory\site\wwwroot"
+
+Write-Output "Downloading Solr $solrVersion to $siteRoot"
 $downloadSource = "https://archive.apache.org/dist/lucene/solr/$solrVersion/$solrName.zip"
-Start-BitsTransfer -Source $downloadSource -Destination \site\wwwroot\solr.zip
-Expand-Archive \wwwroot\solr.zip -DestinationPath \site\wwwroot
+Start-BitsTransfer -Source $downloadSource -Destination "$siteRoot\solr.zip"
+Expand-Archive "$siteRoot\solr.zip" -DestinationPath $siteRoot
