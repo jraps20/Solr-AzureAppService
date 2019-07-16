09f5bec112f5748bd7b374481f4cbab3f8526d31
John Rappel <john_rappel@epam.com>
2019-07-16 11:02:05 -0500

    testing updated deployment

--- Deploy-SolrAzureAppService.ps1
+++ Deploy-SolrAzureAppService.ps1
@@ -4,15 +4,15 @@ Param(
 
 $solrName = "solr-$solrVersion"
 
-Write-Output 'Copy wwwroot folder'
-xcopy wwwroot ..\wwwroot /Y
+#Write-Output 'Copy wwwroot folder'
+#xcopy wwwroot ..\wwwroot /Y
 
 Write-Output 'Setting Security to TLS 1.2'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Output 'Prevent the progress meter from trying to access the console'
 $global:progressPreference = 'SilentlyContinue'
 
-Write-Output 'Downloading Solr $solrVersion'
+Write-Output "Downloading Solr $solrVersion"
 $downloadSource = "https://archive.apache.org/dist/lucene/solr/$solrVersion/$solrName.zip"
 Start-BitsTransfer -Source $downloadSource -Destination \wwwroot\solr.zip
-Expand-Archive \wwwroot\solr.zip -DestinationPath \wwwroot\$solrName
+Expand-Archive \wwwroot\solr.zip -DestinationPath \wwwroot
