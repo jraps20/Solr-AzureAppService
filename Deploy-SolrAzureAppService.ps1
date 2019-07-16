48a46f6497e535c6ca20b772f3b66123f23e628f
John Rappel <john_rappel@epam.com>
2019-07-16 11:18:02 -0500

    updated pathing

--- Deploy-SolrAzureAppService.ps1
+++ Deploy-SolrAzureAppService.ps1
@@ -4,8 +4,8 @@ Param(
 
 $solrName = "solr-$solrVersion"
 
-Write-Output 'Copy wwwroot folder'
-xcopy wwwroot ..\wwwroot /Y
+#Write-Output 'Copy wwwroot folder'
+#xcopy wwwroot ..\wwwroot /Y
 
 Write-Output 'Setting Security to TLS 1.2'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -14,5 +14,5 @@ $global:progressPreference = 'SilentlyContinue'
 
 Write-Output "Downloading Solr $solrVersion"
 $downloadSource = "https://archive.apache.org/dist/lucene/solr/$solrVersion/$solrName.zip"
-Start-BitsTransfer -Source $downloadSource -Destination \wwwroot\solr.zip
-Expand-Archive \wwwroot\solr.zip -DestinationPath \wwwroot
+Start-BitsTransfer -Source $downloadSource -Destination \site\wwwroot\solr.zip
+Expand-Archive \wwwroot\solr.zip -DestinationPath \site\wwwroot
