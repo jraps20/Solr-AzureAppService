300ffe9f52aaed543c636bb84cc6c1977380ca37
John Rappel <john_rappel@epam.com>
2019-07-16 11:56:34 -0500

    further refinements

--- Deploy-SolrAzureAppService.ps1
+++ Deploy-SolrAzureAppService.ps1
@@ -11,12 +11,16 @@ $global:progressPreference = 'SilentlyContinue'
 
 $siteRoot = "D:\home\site\wwwroot"
 
-Write-Output "Downloading Solr $solrVersion to $siteRoot"
+Write-Output "Downloading Solr $solrVersion zip to D:\home\site"
 
 $downloadSource = "https://archive.apache.org/dist/lucene/solr/$solrVersion/$solrName.zip"
 Invoke-WebRequest -Uri $downloadSource -UseBasicParsing -OutFile "..\solr.zip"
-#Start-BitsTransfer -Source $downloadSource -Destination "$siteRoot\solr.zip"
+
+Write-Output "Expanding Solr zip at D:\home\site directory as D:\home\site\$solrName"
 Expand-Archive "..\solr.zip" -DestinationPath "..\"
 
-Write-Output 'Copy web.config to site root'
+Write-Output "Copying contents of D:\home\site\$solrName to D:\home\site\wwwroot"
+Copy-Item -Path "..\$solrName\*" -Destination "..\wwwroot" -recurse -Force -Verbose
+
+Write-Output 'Copy web.config from repository to D:\home\site\wwwroot'
 xcopy web.config ..\wwwroot /Y
